bae422a0d62d32a399d0cadfb78411711ee9ac5c
Lee9Bin <imagen33@daum.net>
2024-05-07 21:07:14 +0900

    feat : member entity 구현

--- src/main/java/com/gyub/omok/member/entity/Member.java
+++ src/main/java/com/gyub/omok/member/entity/Member.java
@@ -1,4 +1,16 @@
 package com.gyub.omok.member.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
+    private String Id;
+    private String password;
+    private String nickName;
+    private String joinDate;
+
 }
